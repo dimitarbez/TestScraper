cfe5683ee36f30ab650795d3036a2450d11d52e7
Dimitar Bezhanovski <dimitarbez@gmail.com>
2021-03-18 13:20:39 +0100

    wait for selectors

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const puppeteer = require('puppeteer')
 const cheerio = require('cheerio')
 const fs = require('fs');
-const { parse } = require('path');
-
 const url = 'https://webscraper.io/test-sites/e-commerce/allinone'
 
 // get the hdd data for the specific item
@@ -11,11 +9,14 @@ async function getHDDListings(page) {
     let html = await page.content()
     let $ = cheerio.load(html)
       
+    await page.waitForSelector('.swatch')
     for (let i = 1; i <= $('.swatch').length; i++) {
         let swatchElement = `.swatches > button:nth-of-type(${i})`
+
         await page.click(swatchElement)
         html = await page.content()
         $ = cheerio.load(html)
+
         let storage = $(swatchElement).text()
         let isAvailable = $(swatchElement).attr('class').includes('disabled') ? false : true
         hddList.push({
@@ -35,9 +36,16 @@ async function getDataFromItem(page, itemUrl) {
     const html = await page.content()
     const $ = cheerio.load(html)
     
+    await page.waitForSelector('.caption')
     item['name'] = $('.caption > h4:nth-of-type(2)').text()
+
+    await page.waitForSelector('.description')
     item['description'] = $('.description').text()
+
+    await page.waitForSelector('.img-responsive')
     item['image_URL'] = 'https://webscraper.io' + $('.img-responsive').attr('src')
+
+    await page.waitForSelector('.ratings')
     item['rating'] = parseInt($('.ratings > p > span').length)
     item['reviews'] = parseInt($('.ratings > p').text().trim().split(' ')[0])
     item['HDD'] = await getHDDListings(page)
@@ -50,14 +58,17 @@ async function listAllItemUrls(page) {
     await page.goto(url)
     const html = await page.content()
     const $ = cheerio.load(html)
+
+    await page.waitForSelector('.title')
     const results = $('.title').map((index, item) => { return 'https://webscraper.io' + $(item).attr('href') }).get()
+
     return results
 }
 
 async function main () {
-    const browser = await puppeteer.launch({headless: true, slowMo: 0})
+    const browser = await puppeteer.launch({headless: true, slowMo: 0 })
     const page = await browser.newPage()
-    page.setViewport({width: 1920, height: 1080})
+    //page.setViewport({width: 1920, height: 1080})
 
     let data = []
 
